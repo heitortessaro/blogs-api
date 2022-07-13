1abecc970a16afc9a23104386576860ecaeb3de7
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-13 18:04:57 -0300

    ci: :card_file_box: Create User model. Add an association of hanMany type with the BlogPosts.

---
+++ src/database/models/user.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+    email: DataTypes.STRING,
+  },
+  {
+    timestamps: false, // remove a obrigatoriedade de utilizar os campos `createdAt` e `updatedAt`
+    tableName: 'User',
+    underscored: true,
+  });
+  
+  // 1:N
+  User.associate = (models) => {
+    User.hasMany(models.BlogPosts,
+      { foreignKey: 'id', as: 'userId' });
+  };
+  return User;
+};
