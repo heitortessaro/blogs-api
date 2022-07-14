4f8dbf3c8be0a96561ddd830d723f4442d93b60d
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-14 14:56:36 -0300

    feat: :sparkles: Add /login route and error middleware

--- src/api.js
+++ src/api.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
-// ...
+const loginRouter = require('./routes/loginRoutes');
+const middlewares = require('./middlewares');
 
 const app = express();
+require('express-async-errors');
 
 app.use(express.json());
 
-// ...
+app.use('/login', loginRouter);
+app.use(middlewares.error);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
