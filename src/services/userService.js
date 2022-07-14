d5948ae84a9a0ac9ce0a9a1692739fe1c016e3c1
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-14 17:37:10 -0300

    ci: :construction: Create userService
    It provides the validateBody, validateNewEmail, and addNewUser. AddNewUser provides a JWT token in case user was correctly add to the db.

---
+++ src/services/userService.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const Joi = require('joi');
+const createError = require('../helpers/createError');
+const jwtService = require('./jwtService');
+const { User } = require('../database/models/index');
+
+const validateBody = async (body) => {
+  const schema = Joi.object({
+    displayName: Joi.string().min(8).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+  });
+  const { error } = await schema.validateAsync(body);
+  if (error) throw error;
+};
+
+const validateNewEmail = async ({ email }) => {
+  const user = await User.findOne({ where: { email } });
+  if (user) {
+    createError(409, 'User already registered');
+  }
+};
+
+const addNewUser = async ({ displayName, email, password, image }) => {
+  const result = await User.create({ displayName, email, password, image });
+  if (result) {
+    const token = jwtService.createToken({ email });
+    return token;
+  } 
+  createError(500, 'Problems adding user to the DB');
+};
+
+module.exports = {
+  validateBody,
+  validateNewEmail,
+  addNewUser,
+};
