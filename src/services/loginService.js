6fe33721e3c256023fab6c37ea668c4117fdbd2d
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-14 14:27:47 -0300

    ci: :construction: Create loginService
    It provides the validateBody, which validate the body information from login requests using Joi. It also provides the validateUserCredentials, which provides a JWT token if credentials are valid.

---
+++ src/services/loginService.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const Joi = require('joi');
+const createError = require('../helpers/createError');
+const jwtService = require('./jwtService');
+const { User } = require('../database/models');
+
+const validateBody = (body) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required().messages({
+        'string.required': 'Some required fields are missing',
+      }),
+    password: Joi.string().required().messages({
+      'string.required': 'Some required fields are missing',
+    }),
+  });
+  const { error } = schema.validate(body);
+  if (error) throw error;
+};
+
+const validateUserCredentials = async ({ email, password }) => {
+    const user = await User.findOne({ where: { email } });
+    if (!user || user.password !== password) {
+      createError(400, 'Invalid fields');
+    }
+    const token = jwtService.createToken({ email });
+    return token;
+};
+
+module.exports = { 
+  validateBody,
+  validateUserCredentials,
+};
